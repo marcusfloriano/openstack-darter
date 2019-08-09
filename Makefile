88e496194f1e98520bba6771dceccc0b7e285b01
Marcus Floriano <marcus.floriano@gmail.com>
2019-08-09 09:32:47 -0300

    update conf for generation docs and update makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 # Minimal makefile for Sphinx documentation
 #
 
+RED=\033[0;31m
+BLUE=\033[0;34m
+LBLUE=\033[1;34m
+LGRAY=\033[0;37m
+NC=\033[0m
+
 # You can set these variables from the command line.
 SPHINXOPTS    =
 SPHINXBUILD   = sphinx-build
@@ -10,6 +16,16 @@ BUILDDIR      = docs/_build
 
 # Put it first so that "make" without argument is like "make help".
 help:
+	@printf "\n"
+	@printf "${LGRAY}Darter (openstack-darter) Help${NC}\n"
+	@printf "\n"
+	@printf "  ${LBLUE}dochelp${NC}     to show help for documentation\n"
+	@printf "  ${LBLUE}servedoc${NC}    to create simple server for show documentations\n"
+	@printf "  ${LBLUE}worker${NC}      to starting the worker for wait jobs for execute\n"
+	@printf "  ${LBLUE}rqdash${NC}      to starting the dashboard for Redis Queue (RQ)\n"
+	@printf "\n"
+
+dochelp:
 	@$(SPHINXBUILD) -M help "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
 
 servedoc:
--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'openstack-darter'
+project = u'Darter'
 copyright = u'2019, Marcus Floriano'
 author = u'Marcus Floriano'
 
@@ -88,7 +88,12 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'github_user': 'marcusfloriano',
+    'github_repo': 'openstack-darter',
+    'description': 'Darter are little jobs for get info about capacity from openstack',
+    'show_related': True
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -102,6 +107,8 @@ html_static_path = ['_static']
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
     '**': [
+        'about.html',
+        'navigation.html',
         'relations.html',  # needs 'show_related': True theme option to display
         'searchbox.html',
     ]
